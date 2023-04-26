1ee8217be60868d66db5036cb1e349f786e83c9f
Syed Ibrahim Maaz <syedibrahim.maaz@gmail.com>
2023-04-27 00:09:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 import streamlit as st
 
-# set maximum upload size to 500MB
-st.set_option('deprecation.showfileUploaderEncoding', False)
-st.set_option('deprecation.showPyplotGlobalUse', False)
-st.set_option('deprecation.showfileUploaderEncoding', False)
-st.set_option('server.maxUploadSize', 500 * 1024 * 1024)
-
 def main():
     st.title("Privacy Preserving Data Mining using Transformation Based Noise Addition")
+    st.write("Authors - Dr. Shashidhar V, Naachiket Pant, Naga Nandan B, Syed Ibrahim Maaz, Shubham Luharuka")
     uploaded_file = st.file_uploader("Choose an excel file", type=["xlsx"])
     if uploaded_file is not None:
         # do something with the uploaded file
-        st.write("File uploaded successfully.")
+        st.write("Database - Pima Indians Diabetes Database")
 
 if __name__ == '__main__':
     main()

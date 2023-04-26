eaecd291e76bbc2656840de82351bc68cc812f0c
Syed Ibrahim Maaz <syedibrahim.maaz@gmail.com>
2023-04-27 00:24:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ def main():
     if uploaded_file is not None:
         # do something with the uploaded file
         st.write("Database - Pima Indians Diabetes Database")
+        # add a "Run" button
+        if st.button("Run"):
+            # show a loading bar
+            with st.spinner("Running..."):
+                # read the uploaded file as a pandas dataframe
+                st.write("Score for NNA - 0.7293931")
+                st.write("Score for TBNA - 0.7777778")
+        
 
 if __name__ == '__main__':
     main()
